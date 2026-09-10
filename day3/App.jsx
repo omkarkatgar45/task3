17dfe84e7593e23a4dd6d989c5c0c1790541c010
Omkar Rajgonda Katgar <omkarkatgar45@gmail.com>
2026-09-10 10:03:56 +0530

    Add files via upload

--- day3/App.jsx
+++ day3/App.jsx
@@ -1,1 +1,18 @@
+import { useState } from 'react'
 
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>My Task Tracker</h1>
+      <p>Count: {count}</p>
+
+      <button onClick={() => setCount(count + 1)}>
+        Click Me
+      </button>
+    </div>
+  )
+}
+
+export default App
