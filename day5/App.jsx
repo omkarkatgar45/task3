07d05de80e231290c2fbe7ac20ac4b9927d169ce
Omkar Rajgonda Katgar <omkarkatgar45@gmail.com>
2026-09-10 10:05:26 +0530

    Add files via upload

--- day5/App.jsx
+++ day5/App.jsx
@@ -1,1 +1,45 @@
+import { useState } from 'react'
 
+function App() {
+  const [tasks, setTasks] = useState([
+    { text: 'Learn React', completed: false },
+    { text: 'Practice JavaScript', completed: true },
+    { text: 'Build a project', completed: false }
+  ])
+
+  const toggleTask = (index) => {
+    const updatedTasks = [...tasks]
+    updatedTasks[index].completed = !updatedTasks[index].completed
+    setTasks(updatedTasks)
+  }
+
+  return (
+    <div>
+      <h1>My Task Tracker</h1>
+
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            <input
+              type="checkbox"
+              checked={task.completed}
+              onChange={() => toggleTask(index)}
+            />
+
+            <span
+              style={{
+                textDecoration: task.completed
+                  ? 'line-through'
+                  : 'none'
+              }}
+            >
+              {task.text}
+            </span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App
