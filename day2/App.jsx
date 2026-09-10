902a306cfb4f31bd33ff48302834dc29446c9d3a
Omkar Rajgonda Katgar <omkarkatgar45@gmail.com>
2026-09-10 09:49:33 +0530

    Add files via upload

--- day2/App.jsx
+++ day2/App.jsx
@@ -1,1 +1,13 @@
+import Greeting from './Greeting'
 
+function App() {
+  return (
+    <div>
+      <h1>My Task Tracker</h1>
+      <Greeting name="Omkar" />
+      <p>Learning React Components and Props</p>
+    </div>
+  )
+}
+
+export default App
