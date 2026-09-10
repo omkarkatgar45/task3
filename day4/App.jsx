2be47789423c606fa33e9c014e162b0ed443043f
Omkar Rajgonda Katgar <omkarkatgar45@gmail.com>
2026-09-10 10:04:43 +0530

    Add files via upload

--- day4/App.jsx
+++ day4/App.jsx
@@ -1,1 +1,30 @@
+import { useState } from 'react'
 
+function App() {
+  const [task, setTask] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    alert(`Task added: ${task}`)
+    setTask('')
+  }
+
+  return (
+    <div>
+      <h1>My Task Tracker</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter a task"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+        />
+
+        <button type="submit">Add Task</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
